6693c4a0daa0c42140b31305fbf139ba152d41a0
Joechen <s900567@yahoo.com.tw>
2022-11-14 22:25:51 +0800

    [Add] apple auth VC

---
+++ SoulBar/Controller/AppleAuthViewController.swift
@@ -0,0 +1,94 @@
+//
+//  AppleAuthViewController.swift
+//  SoulBar
+//
+//  Created by 陳建綸 on 2022/11/14.
+//
+
+import UIKit
+import AuthenticationServices
+import CryptoKit
+
+class AppleAuthViewController: UIViewController {
+    
+    private let signInButton = ASAuthorizationAppleIDButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        signInButton.center = view.center
+    }
+    
+    @objc func didTapSignIn() {
+        
+        let provider = ASAuthorizationAppleIDProvider()
+        
+        let request = provider.createRequest()
+        
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        
+        controller.delegate = self
+        
+        controller.presentationContextProvider = self
+        
+        controller.performRequests()
+        
+    }
+
+}
+
+extension AppleAuthViewController: ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        
+        switch authorization.credential {
+            
+        case let credentials as ASAuthorizationAppleIDCredential:
+            
+            guard let token = credentials.identityToken,  let appleToken = String(data: token, encoding: .utf8) else { return }
+           
+            let id = credentials.user
+
+            let fullName = credentials.fullName
+            
+            let firstName = credentials.fullName?.givenName
+            
+            let lastNmae = credentials.fullName?.familyName
+            
+            let email = credentials.email
+            
+            print(id)
+            print(appleToken)
+            print(fullName)
+            print(firstName)
+            print(lastNmae)
+            print(email)
+            
+        default:
+            break
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+}
+
+extension AppleAuthViewController: ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        
+        return view.window!
+    }
+    
+}
